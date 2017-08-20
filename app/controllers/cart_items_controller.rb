ba00e7b25a08722206835972a2767e2da660b9c2
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-08-21 06:32:03 +0800

    可以修改购物车中商品数量

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,7 +3,12 @@ class CartItemsController < ApplicationController
 
   def update
     cart_item = current_cart.cart_items.find(params[:id])
-    cart_item.update(cart_item_params)
+    if cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      cart_item.update(cart_item_params)
+      flash[:notice] = "成功变更数量"
+    else
+      flash[:warning] = "数量不足以加入购物车"
+    end
 
     redirect_to carts_path
   end
