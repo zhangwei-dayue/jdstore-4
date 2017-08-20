24215cc0dc3a02de728bd47407b285b6812e4bea
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-08-20 17:14:10 +0800

    增加清空购物车功能

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :carts
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
 
   namespace :admin do
     resources :products
