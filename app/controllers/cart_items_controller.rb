5f8e3600edf248320484cae938cb6663a2690d40
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-08-21 06:24:33 +0800

    更改商品数量

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,13 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
+  def update
+    cart_item = current_cart.cart_items.find(params[:id])
+    cart_item.update(cart_item_params)
+
+    redirect_to carts_path
+  end
+
   def destroy
     cart_item = current_cart.cart_items.find(params[:id])
     product = cart_item.product
@@ -8,4 +15,10 @@ class CartItemsController < ApplicationController
     flash[:warning] = "已删除#{product.title}"
     redirect_to carts_path
   end
+
+  private
+
+    def cart_item_params
+      params.require(:cart_item).permit(:quantity)
+    end
 end
